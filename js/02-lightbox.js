f07f5cb94ceffbdfeecf993b70f3b704edd6970d
Lyuda313 <Lyuda313@gmail.com>
2023-07-14 16:52:19 +0300

    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,14 +1,4 @@
 import { galleryItems } from './gallery-items.js';
+// Change code below this line
 
-const galleryContainer = document.querySelector('.gallery');
-
-galleryContainer.innerHTML = galleryItems.map(({preview, original, description}) => 
-        `<li><a class="gallery__item" href="${original}">
-            <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
-        </a></li>`).join("");
-
-const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
-
-
-
-
+console.log(galleryItems);
